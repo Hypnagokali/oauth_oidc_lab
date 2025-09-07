60af0ebe41aaad7fc8a742ad42258163e60eae1e
Stefan Simon <stefan.j.c.simon@gmail.com>
2025-09-07 16:02:53 +0200

    [+] Code to token action

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,42 @@
-use std::env;
+use std::{env, os::linux::raw::stat};
 
-use actix_web::{cookie::Cookie, get, web::{Data, Query, Redirect}, App, HttpResponse, HttpServer, Responder};
+use actix_web::{cookie::Cookie, get, web::{Data, Query}, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use base64::{prelude::{BASE64_URL_SAFE_NO_PAD}, Engine};
 use maud::{html, Markup};
+use reqwest::{redirect::Policy, ClientBuilder};
+use serde::Serialize;
+use sha2::Digest;
 
 struct OAuthConfig {
     client_id: String,
     client_secret: String,
     oauth_user_name: String,
     redirect_uri: String,
+    auth_uri: String,
+    token_uri: String,
+}
+
+#[derive(Serialize)]
+struct CodeRequest {
+    client_id: String,
+    client_secret: String,
+    code: String,
+    redirect_uri: String,
+    code_verifier: String,
+}
+
+impl CodeRequest {
+    pub fn set_code(&mut self, code: &str) {
+        self.code = code.to_string();
+    }
+
+    pub fn set_code_verifier(&mut self, code_verifier: &str) {
+        self.code_verifier = code_verifier.to_string();
+    }
+
+    pub fn to_urlencoded(&self) -> String {
+        serde_urlencoded::to_string(self).unwrap()
+    }
 }
 
 #[derive(serde::Deserialize)]
@@ -17,35 +46,89 @@ struct OAuthResponse {
 }
 
 impl OAuthConfig {
-    fn new(client_id: String, client_secret: String, oauth_user_name: String, redirect_uri: String) -> Self {
+    fn new(client_id: String, client_secret: String, oauth_user_name: String, redirect_uri: String, auth_uri: String, token_uri: String) -> Self {
         OAuthConfig {
             client_id,
             client_secret,
             oauth_user_name,
             redirect_uri,
+            auth_uri,
+            token_uri,
+        }
+    }
+
+    fn token_request(&self) -> CodeRequest {
+        CodeRequest {
+            client_id: self.client_id.clone(),
+            client_secret: self.client_secret.clone(),
+            code: "".to_string(),
+            redirect_uri: self.redirect_uri.clone(),
+            code_verifier: "".to_string(),
         }
     }
 }
 
 #[get("/sso-github")]
-async fn sso_github(response_query: Query<OAuthResponse>) -> impl Responder {
+async fn sso_github(req: HttpRequest, response_query: Query<OAuthResponse>, oauth_config: Data<OAuthConfig>) -> impl Responder {
+    let state_from_auth = &response_query.state;
+    let state_cookie = req.cookie("state");
+
+    if state_cookie.is_none() || state_cookie.unwrap().value() != state_from_auth {
+        // Redirect back to /login/github
+        // max tries: about 3x and then return 401:
+        return HttpResponse::Unauthorized().body("Invalid state parameter");
+    }
+
     println!("SSO GitHub endpoint hit: code: {}, state: {}", response_query.code, response_query.state);
-    HttpResponse::Ok().body("SSO GitHub endpoint hit")
+
+    let mut code_request = oauth_config.token_request();
+    code_request.set_code(&response_query.code);
+    code_request.set_code_verifier("randomstring");
+
+    let res = ClientBuilder::new()
+        .redirect(Policy::none())
+        .build()
+        .unwrap()
+        .post(&oauth_config.token_uri)
+        .body(code_request.to_urlencoded())
+        .send()
+        .await.unwrap();
+
+    println!("Token response: {}", res.status());
+
+    let token = res.text().await.unwrap();
+    
+    HttpResponse::Ok().body(format!("Your token is: {}", token))
 }
 
 #[get("/login/github")]
 async fn login_github(oauth_config: Data<OAuthConfig>) -> impl Responder {
-    let _state_cookie =Cookie::new("state", "random_state_string");
+    println!("GET /login/github");
+    let mut state_cookie = Cookie::new("state", "random_state_string");
+    state_cookie.set_http_only(true);
+    // ToDo: what about cookie lifetime?
+
+    // build PKCE challenge
+    let mut hasher = sha2::Sha256::new();
+    hasher.update(b"randomstring");
+    let hash_bytes = hasher.finalize();
+    let pkce_hash_b64 = BASE64_URL_SAFE_NO_PAD.encode(hash_bytes);
 
     let redirect_uri = urlencoding::encode(&oauth_config.redirect_uri);
-    let s = format!(
-        "https://github.com/login/oauth/authorize?client_id={}&redirect_uri={}&scope=user:email&state=random_state_string",
+    
+    let auth_redirect = format!(
+        "{}?client_id={}&redirect_uri={}&scope=user:email&state=random_state_string&code_challenge_method=S256&code_challenge={}",
+        oauth_config.auth_uri,
         oauth_config.client_id,
-        redirect_uri
+        redirect_uri,
+        pkce_hash_b64
     );
 
-    // TODO: Set state cookie (use HttpResponse)
-    Redirect::to(s).temporary()
+    HttpResponse::TemporaryRedirect()
+        .append_header(("Location", auth_redirect))
+        .append_header(("Cache-Control", "no-store"))
+        .cookie(state_cookie)
+        .finish()
 }
 
 #[get("/")]
@@ -67,17 +150,21 @@ async fn login() -> Markup {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {    
     dotenv::dotenv().ok();
     let client_id = env::var("client_id").expect("client_id must be set");
     let client_secret = env::var("client_secret").expect("client_secret must be set");
-    let oauth_user_name = env::var("oauth-user-name").expect("oauth-user-name must be set");
-    let redirect_uri = env::var("redirect-uri").expect("redirect-uri must be set");
-    let config: Data<OAuthConfig>= Data::new(OAuthConfig::new(client_id, client_secret, oauth_user_name, redirect_uri));
+    let oauth_user_name = env::var("oauth_user_name").expect("oauth_user_name must be set");
+    let redirect_uri = env::var("redirect_uri").expect("redirect_uri must be set");
+    let auth_uri = env::var("auth_uri").expect("auth_uri must be set");
+    let token_uri = env::var("token_uri").expect("token_uri must be set");
+    let config: Data<OAuthConfig>= Data::new(OAuthConfig::new(client_id, client_secret, oauth_user_name, redirect_uri, auth_uri, token_uri));
 
     HttpServer::new(move || {
             App::new()
                 .service(login)
+                .service(login_github)
+                .service(sso_github)
                 .app_data(config.clone())
             
         })
